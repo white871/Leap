a5ecf93789fb51af513cb8bd27520cf06f73d81a
white871 <white871@purdue.edu>
2024-03-01 10:42:14 -0500

    Updated test.py
    Not complete yet

--- test.py
+++ test.py
@@ -1,16 +1,23 @@
-"""
+# Testing for translation without having to use the Raspberry Pi
+# Enter the binary braille keys manually
+import re
 import ast
 
-with open('UEBGrade2Lib', 'r') as f:
+with open('braileLib.txt', 'r') as f:
     data = f.read()
-brailleLib = ast.literal_eval(data)
-"""
+lib = ast.literal_eval(data)
+#print(lib)
+output = []
 while True:
     print("Enter Braille Binary String:")
     braille = input()
     if braille == 'q':
         break;
-    if len(braille) != 6:
+    if len(braille) != 6 or not(re.match('10', braille)):
         print("Invalid, try again")
         continue
     
+    output.append(lib[braille])
+    print(output)
+print(braille)    
+        
