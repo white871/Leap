d8e136d196487e844b7d7431202c4c3f3a5cee93
white871 <white871@purdue.edu>
2024-09-24 18:11:02 -0400

    Progress week 5

--- Transliteration/transliterateBinaryv2.py
+++ Transliteration/transliterateBinaryv2.py
@@ -21,14 +21,15 @@ flags = {
 
 
 def transliterateBin(inputB):
-    global CAP, LEN, TRAN, NME, LIB, NUM, PUN, bLib, b2b, nLib, tlOut, lastOut, i
-    LIB = CAP = LEN = TRAN = NME = NUM = PUN = 0
+    global CAP, LEN, TRAN, NME, LIB, NUM, PUN, FRACT, SPC, bLib, b2b, nLib, tlOut, lastOut, i
+    LIB = CAP = LEN = TRAN = NME = NUM = PUN = FRACT = SPC = 0
     NME = 1 # Remove when ready to test mixed Braille
     tlOut = ""
     lastOut = ""
     iarr = []
     
     i = 0
+    iarr.append("000000")
     while (i <= len(inputB) - 6):
         newStr = ""
         
@@ -36,9 +37,10 @@ def transliterateBin(inputB):
             newStr += inputB[i + j]
         iarr.append(newStr)
         i += 6
+    iarr.append("000000")
     pos = []
-    i = 0
-    while(i < len(iarr)):
+    i = 1
+    while(i < len(iarr) - 1):
         pos = [None] * (len(iarr[i:]) if len(iarr) - i < 5  else 5)
         posLen = len(pos)
         print(f"LENGTH: {len(pos)}")
@@ -48,11 +50,13 @@ def transliterateBin(inputB):
         TRAN = 0
         print(f"POS: {pos}")
         def chkFlags(item):
+            global PUN, CAP, NUM, FRACT, NME
             if item in flags.keys():
                 PUN = 1 if flags[item] == "PUN" else 0
-                CAP = 1 if flags[item] == "CAP" else 0
+                CAP = 1 if (flags[item] == "CAP" and FRACT == 0 and NME == 0) else 0
                 NUM = 1 if flags[item] == "NUM" else 0
-                return 1
+                FRACT = 1 if flags[item] == "FRA" else 0
+                return (PUN or CAP or NUM or FRACT)
             return 0
             
             
@@ -62,12 +66,12 @@ def transliterateBin(inputB):
 
         def NEM(item):
             global tlOut, i, TRAN
+            print(nLib[item])
             if item == '000000':
                 NUM = 0
-                tlOut += " "
-                i += 6
-            
+
             if(item in nLib.keys()):
+                out = ""
                 TRAN = 1
                 tlOut += nLib[item]
                 i += (int) (len(item) / 6)
@@ -87,7 +91,7 @@ def transliterateBin(inputB):
     print(tlOut)
     
 
-transliterateBin("001111010000011000011000000101101000101100101100")           
+transliterateBin("001111011000001011000000000101010000000000001111010011000110000101100001")           
 
 
 
