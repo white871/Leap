ab5ac653f008e5e1f66b2dad9d8fc28e7b68c109
Joseph White <white871@purdue.edu>
2024-11-19 23:09:03 -0500

    Finalized Nemeth and UEB, fixed terminal issues w/ exe

--- Transliteration/transliterateBinaryv2.py
+++ Transliteration/transliterateBinaryv2.py
@@ -1,12 +1,7 @@
 import ast
 import time
 import pdb
-#import tracemalloc
-
-#tracemalloc.start()
-# Open dictionaries
-
-with open('UEBLib.txt', 'r') as bL: 
+with open('brailleLib.txt', 'r') as bL: 
     bLdata = bL.read()
 bLib = ast.literal_eval(bLdata)
 
@@ -51,14 +46,13 @@ NEMFlags = {
     "001001" : "./NUM",       # Minus
     "110011" : "./NUM",       # Vertical Bar
     "001110" : "./NUM",       # Radical Sign
-    "001101001001" : "./NUM",
-    "001001001101" : "./NUM",
+    "001101001001" : "./NUM", # Plus-minus sign
+    "001001001101" : "./NUM", # Minus-plus sign
     "000111" : "./PUN",   
     "000001" : "./CAP",
     "000011" : "./GD1",
     "100111" : "./FRA",
     "000111100111" : "./MIX",
-    #"000111" : "./MIX",
     "000111100011" : "./NEM", # Closing nemeth indicator
     "000111100101" : "./OPENNEM",
     "000001001000" : "./SWI",
@@ -76,7 +70,6 @@ NEMFlags = {
     "001110" : "./RAD",
     "000001100111" : "./COMS",
     "000001001111" : "./COME"
-    #"000001001100" : "./COMH"
 }
 UEBFlags = {
     "000011" : "./GD1",
@@ -91,11 +84,11 @@ UEBFlags = {
 def transliterateBin(inputB):
     global \
         CAP, LEN, TRAN, NME, LIB, NUM, PUN, FRACT, \
-        SPC, CONT, MIX, GD1, SUP, PASTFRACT, SWI, PASTSUP, MUL, PARENTH, RAD, PASTRAD, \
-        bLib, nLib, tlOut, prev, i, iarr, temp
-    LIB = CAP = LEN = TRAN = NME = PARENTH = SUP = NUM = PUN = FRACT = SPC = MUL = CONT = MIX = GD1 = PASTFRACT = PASTSUP = SWI = RAD = PASTRAD = 0
+        SPC, CONT, MIX, GD1, SUP, PASTFRACT, SWI, PASTSUP, MUL, PARENTH, RAD, PASTRAD, TRAN, \
+        bLib, nLib, tlOut, prev, i, iarr, temp, posLen, possible
+    LIB = CAP = LEN = TRAN = NME = PARENTH = SUP = NUM = PUN = FRACT = SPC = MUL = CONT = MIX = GD1 = PASTFRACT = PASTSUP = SWI = RAD = PASTRAD = TRAN = 0
     global flags
-    NME = 1 # Remove when ready to test mixed Braille
+    NME = 1
     tlOut = ""
     prev = ""
     temp = ""
@@ -112,34 +105,30 @@ def transliterateBin(inputB):
         i += 6
     iarr.append("000000")
     iarr.append("000000")
-   #print(f"Input array: {iarr}")
     possible = []
     i = 1
     while(i < len(iarr) - 1):
+        TRAN = 0
         possible = [None] * (len(iarr[i:]) if len(iarr) - i < 5  else 5)
         posLen = len(possible)
-       #print(f"LENGTH: {len(possible)}")
         for k in range(6, -1, -1):
             if k < posLen:
                 possible[posLen - k - 1] = ("".join(iarr[i:i+k+1]))
         
-        #print(f"POSSIBLE: {possible}")
+        print(f"POSSIBLE: {possible}")
 
         def chkFlags(item):
             
             global i, iarr, tlOut, PUN, CAP, NUM, FRACT, NME, CONT, MIX, GD1, SUP, SWI, MUL, RAD, PASTFRACT, PASTSUP, PASTRAD
-           #print(f"NUM: {NUM}")
             if item in nLib.keys():
                 CONT = 1;
             else:
                 CONT = 0;
-                #print(f"{flags[item]}, {FRACT}")
-            
             if not NME and item in UEBFlags.keys():
                 
                 CAP += 1 if UEBFlags[item] == "./CAP" else CAP
                 
-                GD1 = 1 if UEBFlags[item] == "./GD1" else 0
+                GD1 += 1 if UEBFlags[item] == "./GD1" else GD1
                 if UEBFlags[item] == "./NEM":
                     NME = 1
                     if (iarr[i+1] == '000000'):
@@ -150,7 +139,6 @@ def transliterateBin(inputB):
                 if (iarr[i-1] == "000000"):
                     CAP = 0
                     
-               #print(NEMFlags[item])
                 CAP += 1 if (NEMFlags[item] == "./CAP" and FRACT == 0) else CAP
                 PASTFRACT = FRACT
                 PASTSUP = SUP
@@ -161,7 +149,9 @@ def transliterateBin(inputB):
                     tlOut += '('
                 if NEMFlags[item] == "./TERM":
                     RAD = 0
-                    tlOut += ')'
+                    MUL = 0
+                    if prev != u"\N{QUESTIONED EQUAL TO}":
+                        tlOut += ')'
                 
                 if NEMFlags[item] == "./FRA" or NEMFlags[item] == "./COMS" or NEMFlags[item] == "./MIX":
                     FRACT += 1
@@ -171,7 +161,7 @@ def transliterateBin(inputB):
                 if ((NEMFlags[item] == "./NUMIND" or NEMFlags[item] == "./COME") and FRACT != 0):
                     FRACT -= 1
                     tlOut += ')'
-                #FRACT = 0 if (NEMFlags[item] == "./NUMIND" and FRACT == 1 or NEMFlags[item] == "./COMH") else FRACT
+
                 NUM = 1 if ((NEMFlags[item] == "./NUM" or NEMFlags[item] == "./NUMIND"  or NEMFlags[item] == "./SUP" and FRACT == 0) or FRACT or RAD) else NUM
                 PUN = 1 if NEMFlags[item] == "./PUN" else PUN
                 MIX = 1 if NEMFlags[item] == "./MIX" else MIX
@@ -179,69 +169,107 @@ def transliterateBin(inputB):
                 SUP = 1 if NEMFlags[item] == "./SUP" else SUP
                 SUP = 0 if NEMFlags[item] == "./BASE" else SUP
                 SWI = 1 if NEMFlags[item] == "./SWI" else SWI
-               #print(f"past fract: {PASTFRACT}, fract: {FRACT}")
-               #print(f"past rad: {PASTRAD}, rad: {RAD}")
                 if NEMFlags[item] == "./NEM" or SWI:
                     NME = 0
-                    if (iarr[i+2] == '000000'):
-                        i = i+2
+                    if (iarr[i+1] == '000000'):
+                        i = i+1
                     return 1
-                #print(CAP)
-                #print(PUN or CAP or NUM or FRACT)
                 return (item in NEMFlags.keys())
             return 0
             
             
         def UEB(item):
-            global iarr, i, tlOut, TRAN, CAP, GD1, NME, SWI, prev
-           #print(f"PREV: {prev}")
+            global iarr, i, tlOut, TRAN, CAP, GD1, NME, SWI, NUM, prev, posLen, possible
+            punct = [".", ",", "!"]
+            out = ""
             if prev == "./CLOSENEM":
                 GD1 = 1
             else:
                 GD1 = 0
-            spec = 1 # Do all elements have _ or ^?
-            TRAN = 0
-            if SWI and item == "000000":
-                SWI = 0
-                NME = 1
-            if item == "000000" and CAP == 2:
+            if prev == "./CAPTERM":
                 CAP = 0
-            spaceBefore = (iarr[i - 1] == "000000" or iarr[i - 1] in UEBFlags.keys())
-            spaceAfter = (iarr[i + (int)(len(item) / 6)] == "000000" or iarr[i + (int)(len(item) / 6)] in UEBFlags.keys())
+            if item == "000000":
+                NUM = 0
+                if SWI:
+                    SWI = 0
+                    NME = 1
+                if CAP == 2:
+                    CAP = 0
+                if GD1 == 2:
+                    GD1 = 0
+            howlong = (int)(len(item) / 6)
+            if GD1:
+                if howlong > 1:
+                    return
+            spaceBefore = (iarr[i - 1] == "000000")
+            spaceAfter = (iarr[i + howlong] == "000000")
             for perhaps in bLib[item]:
-               #print(perhaps)
                 if not TRAN and not GD1:
                     if "_" in perhaps:
-                       #print("_ found")
+                        expand = False
                         if spaceBefore and spaceAfter:
-                           #print("_ passed")
-                            out = perhaps.replace("_","")
+                            expand = True
+                        if iarr[i + howlong] in bLib.keys():
+                            if (iarr[i - 1] == "000000" or "./" in prev):
+                                if not bLib[iarr[i + howlong]][0].replace("^", "").replace("_", "").isalnum():
+                                    expand = True
+                                    
+                                elif (len(bLib[iarr[i + howlong]]) != 1 and bLib[iarr[i + howlong]][1] in punct and iarr[i + howlong + 1] == "000000"):
+                                    expand = True
+                            
+                        if expand:
+                            out = perhaps.replace("_", "")
+                            prev = perhaps.replace("_", "")
                             TRAN = 1
-                        if iarr[i + (int)(len(item) / 6)] in bLib.keys():
-                            if (iarr[i - 1] == "000000" or "./" in prev) and (not bLib[iarr[i + (int)(len(item) / 6)]][0].replace("^", "").replace("_", "").isalnum()):
-                               #print("_ passed (2nd)")
-                                out = perhaps.replace("_","")
+                    if perhaps[0] == ('^') and perhaps[-1] == '^' and not TRAN:
+                        if i > 0 and i < len(iarr) - 2:
+                            if iarr[i - 1] != "000000" and iarr[i + howlong] != "000000" and not "./" in prev:
+                                out = perhaps.replace("^", "")
+                                prev = perhaps.replace("^", "")
                                 TRAN = 1
-                    elif perhaps[0] == '^' and perhaps[-1] == '^':
-                        if not spaceBefore and not spaceAfter:
-                            out = perhaps.replace("^", "")
-                            TRAN = 1
-                    elif perhaps[0] == '^': 
-                        if not spaceBefore:                           
-                            out = perhaps.replace("^", "")
-                            TRAN = 1
-                    elif perhaps[-1] == '^':
-                        if not spaceAfter:
-                            out = perhaps.replace("^", "")
-                            TRAN = 1
-                    else:
-                        spec = 0
-           #print(f"TRAN: {TRAN}, SPEC: {spec}")
-            if not TRAN and (not spec or len(item) == 6):
-               #print("Final say")
-                out = bLib[item][0].replace("_", "").replace("^","")
+                            else:
+                                continue
+                    if perhaps[0] == ('^') and not TRAN:
+                        if i > 0:
+                            if iarr[i - 1] != "000000":
+                                out = perhaps.replace("^", "")
+                                prev = perhaps.replace("^", "")
+                                TRAN = 1
+                    if perhaps[-1] == ('^') and not TRAN:
+                        if i < len(iarr) - 2:
+                            if iarr[i + howlong] != "000000":
+                                out = perhaps.replace("^", "")
+                                prev = perhaps.replace('^', "")
+                                TRAN = 1
+            if not TRAN and howlong == 5:
+                if possible[1] not in bLib.keys() and possible[2] not in bLib.keys() and possible[3] not in bLib.keys() and possible[4] not in bLib.keys():
+                    out = bLib[item][0]
+                    prev = bLib[item][0]
+                    TRAN = 1
+            stPoint = posLen - howlong
+            if not TRAN and howlong == 4:
+                if possible[stPoint + 1] not in bLib.keys() and possible[stPoint + 2] not in bLib.keys() and possible[stPoint + 3] not in bLib.keys():
+                    out = bLib[item][0]
+                    prev = bLib[item][0]
+                    TRAN = 1
+            if not TRAN and howlong == 3:
+                if possible[stPoint + 1] not in bLib.keys() and possible[stPoint + 2] not in bLib.keys():
+                    out = bLib[item][0]
+                    prev = bLib[item][0]
+                    TRAN = 1
+            if not TRAN and howlong == 2:
+                if possible[stPoint + 1] not in bLib.keys():
+                    out = bLib[item][0]
+                    prev = bLib[item][0]
+                    TRAN = 1
+            if not TRAN and howlong == 1:
+                if ("." in bLib[item] or "," in bLib[item] or "!" in bLib[item]):
+                    out = bLib[item][1]
+                    prev = bLib[item][1]
+                else:
+                    out = bLib[item][0]
+                    prev = bLib[item][0]
                 TRAN = 1
-            
             if TRAN:
                 match CAP: 
                     case 1:
@@ -249,20 +277,18 @@ def transliterateBin(inputB):
                         CAP = 0
                     case 2: 
                         out = out.upper()
-                prev = out
+                    case 3:
+                        out.upper()
+                if NUM:
+                    a_to_n = {"a" : "1", "b" : "2", "c" : "3", "d" : "4", "e" : "5", "f" : "6", "g" : "7", "h" : "8", "i" : "9"}
+                    out = a_to_n[out]
+                print(out)
+                out = out.replace("_", "").replace("^","")
                 tlOut += out
                 i += (int)(len(item) / 6)
-           #print(tlOut)
 
         def NEM(item):
             global tlOut, i, iarr, TRAN, NUM, CAP, SPC, PUN, SUP, MUL, PASTFRACT, FRACT, PASTRAD, prev, temp, PARENTH
-            #print(f"{item} passed, checking")
-            #print(f"PREV: {prev}")
-            #print(f"PUN: {PUN}")
-            #print(f"CAP: {CAP}")
-            #print(nLib[item])
-            #print(SUP)
-            # Ordered Pairs
            
             if item == "000000" or item == "000001000000":
                 if not PARENTH:
@@ -277,24 +303,18 @@ def transliterateBin(inputB):
             if prev == "./BASE" and PASTSUP:
                 tlOut += ")"
                     
-                    
-
-                
-            #if prev == "./NUMIND" and PASTFRACT:
-               # tlOut += ")"
             if prev == "./LINE":
                 tlOut += r'$\overline{temp}$'
                 temp = ""
                 MUL = 0
-            #print(f"NUM: {NUM}")
             if PUN and item in punctuation.keys():
                 out = punctuation[item]
             elif NUM and item in nums.keys(): 
-                #print("NUM!")
+
                 out = nums[item]
             else:
                 out = nLib[item]
-        
+            print(out)
             # Parentheses, Brackets, & Braces! Oh my!        
             if out == "(":
                 PARENTH = 1
@@ -316,6 +336,7 @@ def transliterateBin(inputB):
                     out = '\033[1m' + ']' + '\033[0m'
             if out.isnumeric() and prev == "./CAP":
                 out = "," + out
+                
             # Lines, bars, rays, not statement to exclude long dashes
             if out[0] == "_" and out[1] != "_" : 
                 if MUL:
@@ -338,79 +359,54 @@ def transliterateBin(inputB):
                     CAP = 0
                 case 2: 
                     out = out.capitalize()
-            #if SUP:
-            #    tlOut += "^" + out
-            #else:
             
             if item == "001100" and not FRACT:
                 out = "/"
             prev = out
-           #print(out)
             if MUL and out.isalnum():
-               #print("Stored in temp.")
                 temp += out
             else:
                 tlOut += out
-                
-           #print(tlOut)
             i += (int) (len(item) / 6)
-       #print(iarr[i-1])
         if (iarr[i-1] == "000000"):
             SPC = 1
         else:
             SPC = 0
         for item in possible:
             print(f"Checking {item}, length {len(item)}")
-           #print(f"NEM: {NME}")
+            print(f"NEM: {NME}")
             
             if not chkFlags(item) or CONT:
-                #print(f"Flag not found, (or flag raised but continue is {CONT})")
-                    #print(f"{item} is not a flag.")
                 CONT = 0
                 if NME and (item in nLib.keys() or item in nums.keys()):
-                        #print(f"Checking nemeth: {nLib[item]}")
                     NEM(item)
                     break
                 elif not NME and item in bLib.keys():
-                        #print(f"Checking UEB: {nLib[item]}")
                     UEB(item)
                     if not TRAN:
-                       #print("Not found!")
                         continue
+                    break
                 else: 
                     if len(item) == 6:
                         NME = not NME # Try other?
             else:
                 i += (int) (len(item) / 6)
                 prev = UEBFlags[item] if not NME else NEMFlags[item]
-               #print(f"Item is a flag {prev}")
                 break
 
             print(tlOut)
-    #print(tlOut)
+
     return tlOut[:-1]
     
-#pdb.set_trace()
-start_time = time.time()
+""" 
 with open('nemethTestCasetest.txt', 'w', encoding = 'utf-8') as out:
-    with open('nemethTestCaseIn.txt', 'r') as test:
-        for line in test:
-            print(line)
-            if line[0] == 'L':
-                out.write(line)
-            else:
-                #try:
-                out.write(transliterateBin(line) + '\n')
-                #except:
-                #    pass
-#braille = input("Input Binary input.\n")
-#braille = "000001011101000000101100100000101111101010111010000000010111000000111000100000110000101010111010000000111111000000111101000000011111000000011101000000111100000000111011000000011101000000111001010100111000111000100000110110100010"
-
-#transliterateBin(braille)  
-#print("-------PROCESS COMPLETE--------------------------------------")         
-print(f"PROCESS COMPLETED IN {(time.time() - start_time) * 1000} ms")
-#print(f"MEMORY USAGE: {tracemalloc.get_traced_memory()}")
-#tracemalloc.stop()
+   with open('nemethTestCaseIn.txt', 'r') as test:
+       for line in test:
+           print(line)
+           if line[0] == 'L':
+               out.write(line)
+           else:
+               out.write(transliterateBin(line) + '\n') """
 
 
 
